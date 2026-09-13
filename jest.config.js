45991d368426a97f814534d6196e1bb90824cb42
DefMaks <5427308+DefMaks@users.noreply.github.com>
2026-09-13 12:22:37 +0000

    fix missing babel and twiga fixes

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
-module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
-  transformIgnorePatterns: ["/node_modules/"],
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'node',
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  modulePathIgnorePatterns: ['<rootDir>/.next/'],
 };
+
+module.exports = createJestConfig(customJestConfig);
